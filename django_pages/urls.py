2bab75f098703ec0800f3509b51da98c344ffec2
Max <wreakthunder22@gmail.com>
2024-04-04 16:06:43 -0500

    initial commit

---
+++ django_pages/urls.py
@@ -0,0 +1,8 @@
+# django_pages/urls.py
+from django.contrib import admin
+from django.urls import path, include # new
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('pages.urls')), # new
+]
